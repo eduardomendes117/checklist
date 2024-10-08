607845df314a4528f5f98288fb8ea489c0c21ae6
DuduDev <107651217+eduardomendes117@users.noreply.github.com>
2024-10-08 07:58:35 -0300

    update

---
+++ js/verificarListaVazia.js
@@ -0,0 +1,9 @@
+const mensagemListaVazia = document.getElementById("mensagem-lista-vazia")
+
+export function verificarListaVazia(Lista) {
+    if (Lista.querySelectorAll("li").length === 0) {
+        mensagemListaVazia.style.display = "block";
+    } else {
+        mensagemListaVazia.style.display = "none";
+    }
+}
